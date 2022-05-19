fa09b2fe70afd4a91ad777de6a09bcf2151e6d5c
Douglas <douglas.n.silva30@gmail.com>
2022-05-18 22:23:09 -0300

    Implementado CRUD

---
+++ src/main/java/com/projetologistica/logisticaapi/controller/ClienteController.java
@@ -0,0 +1,64 @@
+package com.projetologistica.logisticaapi.controller;
+
+import com.projetologistica.logisticaapi.domain.repository.ClienteRepository;
+import com.projetologistica.logisticaapi.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    // Listar todos os clientes
+    @GetMapping()
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    // Buscar apena por cliente expecifico
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Adiciona um cliente
+    @PostMapping("/adicionar")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    // Atualizar cliente
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        if (!clienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+
+        } else {
+            cliente.setId(id);
+            cliente = clienteRepository.save(cliente);
+            return ResponseEntity.ok(cliente);
+        }
+    }
+
+    // Deletar
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        if (!clienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        } else {
+            clienteRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+
+    }
+}
---
+++ src/main/java/com/projetologistica/logisticaapi/domain/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.projetologistica.logisticaapi.domain.repository;
+
+import com.projetologistica.logisticaapi.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    List<Cliente> findByNome(String nome);
+}
